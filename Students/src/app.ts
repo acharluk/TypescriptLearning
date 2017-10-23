49ddd014de72f19e2392cd1ebdb40e665522c838
ACharLuk <acharluk@gmail.com>
2017-10-24 01:03:48 +0200

    Basic test app

--- Students/src/app.ts
+++ Students/src/app.ts
@@ -1,5 +1,12 @@
 import { Student } from './Student'
+import { Teacher } from "./Teacher";
+import { Course } from "./Course"
 
 let alex = new Student("Alex")
 
-console.log(alex.name)
+let mike = new Teacher("Mike")
+
+let math_i = new Course("Math I")
+
+math_i.setTeacher(mike)
+math_i.addStudent(alex)
