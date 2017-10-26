fcec0306de3b7bc46893554a06bb7aba97b518c5
ACharLuk <acharluk@gmail.com>
2017-10-27 01:29:52 +0200

    Test with two courses

--- Students/src/index.ts
+++ Students/src/index.ts
@@ -4,12 +4,15 @@ import { CourseView } from './views/CourseView'
 import { Course } from './core/Course'
 
 let myCourse = new Course("Math I")
-
 myCourse.addStudent([{name: "Alex"}])
-
 let courseTest = new CourseView(myCourse)
-// courseTest.show(myCourse)
+
+let myCourse2 = new Course("Physics II")
+myCourse2.addStudent([{name: "Combo"}])
+let courseTest2 = new CourseView(myCourse2)
+
 
 let mainV = new MainView()
 mainV.add(courseTest)
+mainV.add(courseTest2)
 mainV.show()
