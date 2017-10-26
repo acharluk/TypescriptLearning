0674baec96189e86161af2de66bc076295c997ba
ACharLuk <acharluk@gmail.com>
2017-10-26 09:46:25 +0200

    Update bundle.js

--- Students/bundle.js
+++ Students/bundle.js
@@ -13,9 +13,30 @@ exports.Student = Student;
 Object.defineProperty(exports, "__esModule", { value: true });
 // Web interface
 const Student_1 = require("./Student");
-let s = new Student_1.Student("Alex");
-let testp = document.createElement('h1');
-testp.textContent = s.name;
-document.body.appendChild(testp);
+let students = [];
+let addStudent_input = document.createElement('input');
+let addStudent_button = document.createElement('button');
+let refreshStudents_button = document.createElement('button');
+let addStudent = () => {
+    let newStudent = new Student_1.Student(addStudent_input.value);
+    students.push(newStudent);
+    console.log("Added new student: " + newStudent.name);
+};
+let refreshStudents = () => {
+    console.log("Refreshing student list");
+    students.forEach(v => {
+        let s = document.createElement('p');
+        s.textContent = v.name;
+        document.body.appendChild(s);
+        console.log(v.name);
+    });
+};
+addStudent_button.onclick = addStudent;
+addStudent_button.textContent = "Add Student";
+refreshStudents_button.onclick = refreshStudents;
+refreshStudents_button.textContent = "Refresh student list";
+document.body.appendChild(addStudent_input);
+document.body.appendChild(addStudent_button);
+document.body.appendChild(refreshStudents_button);
 
 },{"./Student":1}]},{},[2]);
