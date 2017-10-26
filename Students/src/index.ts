2b2d672af22de8b8591d582bcfdb35db9f01cf9d
ACharLuk <acharluk@gmail.com>
2017-10-26 09:46:15 +0200

    Student list!

--- Students/src/index.ts
+++ Students/src/index.ts
@@ -5,16 +5,31 @@ let students: Student[] = []
 
 let addStudent_input = document.createElement('input')
 let addStudent_button = document.createElement('button')
+let refreshStudents_button = document.createElement('button')
 
 let addStudent = () => {
     let newStudent = new Student(addStudent_input.value)
     students.push(newStudent)
+    console.log("Added new student: " + newStudent.name)
 }
 
+let refreshStudents = () => {
+    console.log("Refreshing student list")
+    students.forEach(v => {
+        let s = document.createElement('p')
+        s.textContent = v.name
+        document.body.appendChild(s)
+        console.log(v.name)
+    })
+}
 
-addStudent_button.click = addStudent
+addStudent_button.onclick = addStudent
 addStudent_button.textContent = "Add Student"
+
+refreshStudents_button.onclick = refreshStudents
+refreshStudents_button.textContent = "Refresh student list"
 
 
 document.body.appendChild(addStudent_input)
 document.body.appendChild(addStudent_button)
+document.body.appendChild(refreshStudents_button)
