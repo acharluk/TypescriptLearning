b1fe6aced58268c82726e2310ffa4f6698430dd7
ACharLuk <acharluk@gmail.com>
2017-10-28 00:42:52 +0200

    Update bundle.js

--- Students/bundle.js
+++ Students/bundle.js
@@ -1,49 +1,6 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Student_1 = require("./Student");
-class Course {
-    constructor(name, description) {
-        this.name = name;
-        this.description = description ? description : "No description provided";
-        this.students = [];
-    }
-    setTeacher(teacher) {
-        this.teacher = teacher;
-    }
-    addStudent(student) {
-        if (student instanceof Student_1.Student) {
-            this.students.push(student);
-        }
-        else {
-            student.forEach(v => this.students.push(v));
-        }
-    }
-    printInfo() {
-        console.log("Course name: " + this.name);
-        console.log("Description: " + this.description);
-        console.log("Teacher: " + this.teacher.name);
-        console.log("Students: ");
-        this.students.forEach((v, i) => {
-            console.log(i + ": " + v.name);
-        });
-    }
-}
-exports.Course = Course;
-
-},{"./Student":2}],2:[function(require,module,exports){
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Student {
-    constructor(name) {
-        this.name = name;
-    }
-}
-exports.Student = Student;
-
-},{}],3:[function(require,module,exports){
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 class CourseView {
     constructor(course, root) {
         this.course = course;
@@ -64,7 +21,7 @@ class CourseView {
 }
 exports.CourseView = CourseView;
 
-},{}],4:[function(require,module,exports){
+},{}],2:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class MainView {
@@ -72,10 +29,10 @@ class MainView {
         this.root = root ? root : document.body;
         this.courseOptions = [];
         this.courseOptions_html = document.createElement('select');
-        this.courseViews_html = document.createElement('div');
+        this.selectedCourseView_html = document.createElement('div');
     }
     add(cView) {
-        cView.root = this.courseViews_html;
+        cView.root = this.selectedCourseView_html;
         this.courseOptions.push(cView);
     }
     show() {
@@ -85,33 +42,44 @@ class MainView {
             let option = document.createElement('option');
             option.textContent = v.course.name;
             this.courseOptions_html.onchange = () => {
-                this.courseOptions[0].show();
+                let index = this.courseOptions_html.selectedIndex;
+                this.courseOptions[index].show();
                 console.log("onchange!");
             };
             this.courseOptions_html.appendChild(option);
         });
         this.root.appendChild(this.courseOptions_html);
-        this.root.appendChild(this.courseViews_html);
+        this.root.appendChild(this.selectedCourseView_html);
     }
 }
 exports.MainView = MainView;
 
-},{}],5:[function(require,module,exports){
+},{}],3:[function(require,module,exports){
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // Web interface
 const MainView_1 = require("./views/MainView");
 const CourseView_1 = require("./views/CourseView");
-const Course_1 = require("./core/Course");
-let myCourse = new Course_1.Course("Math I");
-myCourse.addStudent([{ name: "Alex" }]);
-let courseTest = new CourseView_1.CourseView(myCourse);
-let myCourse2 = new Course_1.Course("Physics II");
-myCourse2.addStudent([{ name: "Combo" }]);
-let courseTest2 = new CourseView_1.CourseView(myCourse2);
+let db = [
+    {
+        "name": "Math I",
+        "description": "No description provided",
+        "students": [
+            { "name": "Alex" }
+        ]
+    },
+    {
+        "name": "Physics II",
+        "description": "No description provided",
+        "students": [
+            { "name": "Combo" }
+        ]
+    }
+];
 let mainV = new MainView_1.MainView();
-mainV.add(courseTest);
-mainV.add(courseTest2);
+db.forEach(v => {
+    mainV.add(new CourseView_1.CourseView(v));
+});
 mainV.show();
 
-},{"./core/Course":1,"./views/CourseView":3,"./views/MainView":4}]},{},[5]);
+},{"./views/CourseView":1,"./views/MainView":2}]},{},[3]);
