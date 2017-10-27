768bf5b781abdd017a946450f606592826a7ecff
ACharLuk <acharluk@gmail.com>
2017-10-28 00:42:45 +0200

    Easier db loading for test

--- Students/src/index.ts
+++ Students/src/index.ts
@@ -3,16 +3,27 @@ import { MainView } from './views/MainView'
 import { CourseView } from './views/CourseView'
 import { Course } from './core/Course'
 
-let myCourse = new Course("Math I")
-myCourse.addStudent([{name: "Alex"}])
-let courseTest = new CourseView(myCourse)
-
-let myCourse2 = new Course("Physics II")
-myCourse2.addStudent([{name: "Combo"}])
-let courseTest2 = new CourseView(myCourse2)
-
+let db: any[] = [
+    {
+        "name": "Math I",
+        "description": "No description provided",
+        "students": [
+            { "name": "Alex" }
+        ]
+    },
+    {
+        "name": "Physics II",
+        "description": "No description provided",
+        "students": [
+            { "name": "Combo" }
+        ]
+    }
+]
 
 let mainV = new MainView()
-mainV.add(courseTest)
-mainV.add(courseTest2)
+
+db.forEach(v => {
+    mainV.add(new CourseView(v))
+})
+
 mainV.show()
