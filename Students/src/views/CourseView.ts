eb92a4db161e119b0eb6c750d6937bfc2a9a523e
ACharLuk <acharluk@gmail.com>
2017-10-29 01:34:11 +0200

    Remove old selection

--- Students/src/views/CourseView.ts
+++ Students/src/views/CourseView.ts
@@ -15,8 +15,10 @@ export class CourseView {
     show() {
         while(this.students_html.lastChild)
             this.students_html.removeChild(this.students_html.lastChild)
+        while(this.root.lastChild)
+            this.root.removeChild(this.root.lastChild)
+
         this.course.students.forEach(v => {
-            console.log(v)
             let option = document.createElement('option')
             option.textContent = v.name
             
