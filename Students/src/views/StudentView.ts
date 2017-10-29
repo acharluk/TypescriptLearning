3c56a90c9f1a201cacdf471e27b741bcd97d1287
ACharLuk <acharluk@gmail.com>
2017-10-29 02:24:43 +0100

    StudentView show function

--- Students/src/views/StudentView.ts
+++ Students/src/views/StudentView.ts
@@ -1,15 +1,28 @@
 import { Student } from "./../core/Student";
 
-class StudentView {
+export class StudentView {
     root: HTMLElement
 
     student: Student
     student_html: HTMLElement
 
-    constructor(student: Student) {
+    constructor(student: Student, root ?: HTMLElement) {
         this.student = student
+        this.root = root ? root : document.body
+        this.student_html = document.createElement('div')
     }
 
-    
+    show() {
+        while (this.root.lastChild)
+            this.root.removeChild(this.root.lastChild)
+
+        this.student.marks.forEach(v => {
+            let mark_html = document.createElement('p')
+            mark_html.textContent = v.toString()
+            this.student_html.appendChild(mark_html)
+        })
+
+        this.root.appendChild(this.student_html)
+    }
 
 }
