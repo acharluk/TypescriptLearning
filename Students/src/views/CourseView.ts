e91fa4c345d32ddb7a14983799b1577126908777
ACharLuk <acharluk@gmail.com>
2017-10-29 02:25:02 +0100

    Add StudentView control to CourseView

--- Students/src/views/CourseView.ts
+++ Students/src/views/CourseView.ts
@@ -1,15 +1,25 @@
 import { Course } from './../core/Course'
+import { StudentView } from "./StudentView";
 
 export class CourseView {
     root: HTMLElement
 
     course: Course
-    students_html: HTMLElement
+    students_html: HTMLSelectElement
+    students_views: StudentView[]
+    selectedStudentView_html: HTMLElement
 
     constructor(course: Course, root ?: HTMLElement) {
         this.course = course
         this.root = root ? root : document.body
         this.students_html = document.createElement('select')
+        this.students_views = []
+        this.selectedStudentView_html = document.createElement('div')
+        this.course.students.forEach(v => {
+            let newSV = new StudentView(v)
+            newSV.root = this.selectedStudentView_html
+            this.students_views.push(newSV)
+        })
     }
 
     show() {
@@ -24,6 +34,13 @@ export class CourseView {
             
             this.students_html.appendChild(option)
         })
+
+        this.students_html.onchange = () => {
+            let index = this.students_html.selectedIndex
+            this.students_views[index].show()
+        }
+
         this.root.appendChild(this.students_html)
+        this.root.appendChild(this.selectedStudentView_html)
     }
 }
