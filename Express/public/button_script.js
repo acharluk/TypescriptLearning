ae72a7447bce2a359e741b9496914ce721f009a1
ACharLuk <acharluk@gmail.com>
2017-11-13 14:07:18 +0100

    Made button script more interesting

--- Express/public/button_script.js
+++ Express/public/button_script.js
@@ -1,3 +1,10 @@
+let list = document.getElementById('pressed_list');
+
 function press() {
     console.log("Pressed!");
+
+    let li = document.createElement('li');
+    li.textContent = "Pressed! Here's a random number: " + Math.random();
+
+    list.appendChild(li);
 }
