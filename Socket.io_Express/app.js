234a27d786c3501c867aa68cea6d7363708d2eab
ACharLuk <acharluk@gmail.com>
2017-11-14 01:09:03 +0100

    Added port and public folder

--- Socket.io_Express/app.js
+++ Socket.io_Express/app.js
@@ -2,3 +2,8 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io').listen(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(port, () => console.log('Server started on port: ' + port));
