fb9b8f719a518de3d097c17b855f15bebe9ed55c
ACharLuk <acharluk@gmail.com>
2017-11-15 01:08:17 +0100

    Add get_random & give_random events

--- Socket.io_Express/app.js
+++ Socket.io_Express/app.js
@@ -10,8 +10,15 @@ io.on('connection', socket => {
     socket.on('disconnect', () => console.log("User disconnected"));
 
     socket.emit('connected');
+    socket.on('get_random', () => {
+        socket.emit('give_random', getRandomNumber());
+    })
 
     console.log("New connection!");
 })
+
+function getRandomNumber() {
+    return Math.random();
+}
 
 http.listen(port, () => console.log("Server started on port: " + port));
