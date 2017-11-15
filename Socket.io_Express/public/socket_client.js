d5b00b7b5cac86d34fc3c625f800a8f367bcfd9c
ACharLuk <acharluk@gmail.com>
2017-11-15 01:13:20 +0100

    Put random numbers on the list

--- Socket.io_Express/public/socket_client.js
+++ Socket.io_Express/public/socket_client.js
@@ -1,8 +1,14 @@
 const socket = io();
 
+let random_list = document.getElementById("random_list")
+
 socket.on('connected', () => console.log("Connected to server!"));
 
-socket.on('give_random', data => console.log(data));
+socket.on('give_random', data => {
+    let li = document.createElement('li');
+    li.textContent = data;
+    random_list.appendChild(li);
+});
 
 function requestRandom() {
     socket.emit('get_random');
