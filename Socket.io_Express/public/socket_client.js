62d833248d90a68559c683c331262a10c37818fc
ACharLuk <acharluk@gmail.com>
2017-11-15 01:09:34 +0100

    Handle get_random & give_random on client

--- Socket.io_Express/public/socket_client.js
+++ Socket.io_Express/public/socket_client.js
@@ -1,3 +1,9 @@
 const socket = io();
 
 socket.on('connected', () => console.log("Connected to server!"));
+
+socket.on('give_random', data => console.log(data));
+
+function requestRandom() {
+    socket.emit('get_random');
+}
