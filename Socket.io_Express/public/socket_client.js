dcaa0b57733e9a2258780d3d754c743a8b3e4976
ACharLuk <acharluk@gmail.com>
2017-11-16 01:58:01 +0100

    Refractor to messages

--- Socket.io_Express/public/socket_client.js
+++ Socket.io_Express/public/socket_client.js
@@ -1,13 +1,13 @@
 const socket = io();
 
-let random_list = document.getElementById("random_list")
+let messages = document.getElementById("random_list")
 
 socket.on('connected', () => console.log("Connected to server!"));
 
-socket.on('give_random', data => {
+socket.on('message', data => {
     let li = document.createElement('li');
     li.textContent = data;
-    random_list.appendChild(li);
+    messages.appendChild(li);
 });
 
 socket.emit('login', { nick: prompt("Enter your nick:") });
