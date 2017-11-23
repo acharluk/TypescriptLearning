f602c233da85e24ba15dd93703c04699d5d4019c
ACharLuk <acharluk@gmail.com>
2017-11-23 14:38:17 +0100

    Simpler help function

--- Socket.io_Express/commands.js
+++ Socket.io_Express/commands.js
@@ -3,5 +3,5 @@ module.exports = {
     'time': { description: "Tells the current time", run() { return new Date().toLocaleTimeString() } },
     'date': { description: "Tells the current date", run() { return new Date().toLocaleDateString() } },
     'random': { description: "Returns a random number between the two arguments given", run(args) { return Math.floor(Math.random() * (args[2] - args[1])) + parseInt(args[1]) } },
-    'help': { description: "Gives help about any command", run(args) { if (module.exports[args[1]]) return args[1] + ": " + module.exports[args[1]].description; else return "Command not found" } }
+    'help': { description: "Gives help about any command", run(args) { return module.exports[args[1]] ? args[1] + ": " + module.exports[args[1]].description : "Command not found" } }
 }
