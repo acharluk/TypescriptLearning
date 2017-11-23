4d5ba1f2361a99858d7545ee92ba361ead721037
ACharLuk <acharluk@gmail.com>
2017-11-23 11:18:54 +0100

    Add get random number command

--- Socket.io_Express/commands.js
+++ Socket.io_Express/commands.js
@@ -1,5 +1,6 @@
 module.exports = {
     'ping': { description: "Just a test command that returns pong!", run() { console.log("PONG!"); return "pong!" } },
     'time': { description: "Tells the current time", run() { return new Date().toLocaleTimeString() } },
-    'date': { description: "Tells the current date", run() { return new Date().toLocaleDateString() } }
+    'date': { description: "Tells the current date", run() { return new Date().toLocaleDateString() } },
+    'random': { description: "Returns a random number between the two arguments given", run(args) { return Math.floor(Math.random() * (args[2] - args[1])) + parseInt(args[1]) } }
 }
