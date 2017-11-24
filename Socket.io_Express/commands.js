dccbc4e8f03116dd3267aa3c5be7cbe99c3191a6
ACharLuk <acharluk@gmail.com>
2017-11-25 00:09:49 +0100

    Add `specs` command

--- Socket.io_Express/commands.js
+++ Socket.io_Express/commands.js
@@ -1,7 +1,10 @@
+const os = require('os')
+
 module.exports = {
     'ping': { description: "Just a test command that returns pong!", run() { return "pong!" } },
     'time': { description: "Tells the current time", run() { return new Date().toLocaleTimeString() } },
     'date': { description: "Tells the current date", run() { return new Date().toLocaleDateString() } },
     'random': { description: "Returns a random number between the two arguments given", run(args) { return Math.floor(Math.random() * (args[2] - args[1])) + parseInt(args[1]) } },
-    'help': { description: "Gives help about any command", run(args) { return module.exports[args[1]] ? args[1] + ": " + module.exports[args[1]].description : "Command not found" } }
+    'help': { description: "Gives help about any command", run(args) { return module.exports[args[1]] ? args[1] + ": " + module.exports[args[1]].description : "Command not found" } },
+    'specs': { description: "Gives server specs", run() { return `${os.cpus().length}x ${os.cpus()[0].model} | RAM: Total=${Math.floor(os.totalmem()/(1024*1024))}MB Free=${Math.floor(os.freemem()/(1024*1024))}MB | OS: ${os.type()} ${os.platform()} ${os.arch()} ${os.release()}` } }
 }
