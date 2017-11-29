4db7c34d7d347364eaf574c75159b335a8d7221b
ACharLuk <acharluk@gmail.com>
2017-11-29 22:53:51 +0100

    Remove html from showing up on the message

--- Socket.io_Express/commands.js
+++ Socket.io_Express/commands.js
@@ -7,5 +7,5 @@ module.exports = {
     'random': { description: "Returns a random number between the two arguments given", run(args) { return Math.floor(Math.random() * (args[2] - args[1])) + parseInt(args[1]) } },
     'help': { description: "Gives help about any command", run(args) { return module.exports[args[1]] ? args[1] + ": " + module.exports[args[1]].description : "Command not found" } },
     'specs': { description: "Gives server specs", run() { return `${os.cpus().length}x ${os.cpus()[0].model} <br/> RAM: Total=${Math.floor(os.totalmem()/(1024*1024))}MB Free=${Math.floor(os.freemem()/(1024*1024))}MB <br/> OS: ${os.type()} ${os.platform()} ${os.arch()} ${os.release()}` } },
-    'html': { description: "Let's you write html on a message", run(...args) { return args } }
+    'html': { description: "Let's you write html on a message", run(args) { return args.splice(1,1) } }
 }
